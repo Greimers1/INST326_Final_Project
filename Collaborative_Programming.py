26dbfe6e491ae5357e2df63d46d85bcd7e3553ac
Adam Blum <ablum03@terpmail.umd.edu>
2023-11-08 15:43:40 -0500

    scorekeep and display

--- Collaborative_Programming.py
+++ Collaborative_Programming.py
@@ -3,7 +3,7 @@ import re
 """
 """
 class Game:
-    """ Represents a game. 
+    """ Represents a game. o 
     """
     
     
@@ -17,6 +17,45 @@ class Game:
             a_number = match.group('a_number')
             answer = match.group('answer')
     
+
+
+class ScoreKeeper:
+    def __init__(self, player_score, computer_score):
+        self.player_score = 0
+        self.computer_score = 0 
+        
+    def get_player_score(self):
+        return self.player_score
+    
+    def get_computer_score(self):
+        return self.computer_score
+        
+    def display_score(self, last_question):
+        pscore = self.get_player_score
+        cscore = self.get_computer_score
+        
+        if pscore < 0 or cscore < 0:
+                print("Invalid scores")
+                return
+        
+        # This will print the scores and the result if the game is over and last question is true
+        if last_question == True:
+            
+            if pscore == cscore:
+                print(f"You have tied. The final score was: \n Player: {pscore}\nComputer: {cscore}")
+                return
+
+            player_win = True if pscore > cscore else False
+        
+            if player_win:
+                print(f"Congrats the player won!! The final score was: \n Player: {pscore}\nComputer: {cscore}")
+            else: 
+                 print(f"Sorry the computer won :( The final score was: \n Player: {pscore}\nComputer: {cscore}")
+                 
+        else:
+            # This will print if the game is still going and the display_score function is called
+            print(f"The score is: \n Player: {pscore}\nComputer: {cscore}")
+        
 
 
 class Driver : 
