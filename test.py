7a3b978e14b0d32d13fa004c92042c49e43378c0
Greimers1 <ahmedelhag877@gmail.com>
2023-12-01 00:05:33 -0500

    test is still not done

--- test.py
+++ test.py
@@ -5,42 +5,88 @@ import random
 
 class TriviaGame:
     def __init__(self, line):
-        pattern = r"""(?x)Q(?P<q_number>\d+): (?P<question>.*?[?])\nA(?P<a_number>\d+): (?P<answer>.*?)$"""
-        match = re.search(pattern, line, re.IGNORECASE)
-        if not match:
-            raise ValueError(f"Invalid format for trivia question: {line}")
-        
-        self.q_number = int(match.group(1))
-        self.question = match.group(2)
-        self.a_number = int(match.group(3))
-        self.answer = match.group(4)
+        question_pattern = r"Q(\d+): (.*)$"
+        answer_pattern = r"A(\d+): (.*)$"
+
+        question_match = re.match(question_pattern, line)
+        answer_match = re.match(answer_pattern, line)
+
+        if question_match:
+            self.q_number = int(question_match.group(1))
+            self.question = question_match.group(2)
+            self.answer = None
+        elif answer_match:
+            self.a_number = int(answer_match.group(1))
+            self.answer = answer_match.group(2)
+            self.q_number = None
+            self.question = None
+        else:
+            raise ValueError(f"Invalid format for trivia line: {line}")
 
     def display_question(self):
-        print(f"Question {self.q_number}: {self.question}")
+        if hasattr(self, 'question') and self.question:
+            print(f"Question {self.q_number}: {self.question}")
+        elif hasattr(self, 'answer') and self.answer:
+            print(f"Answer {self.a_number}: {self.answer}")
 
     def get_answer(self):
-        return input("Your answer: ").strip()
+        if hasattr(self, 'q_number') and self.q_number is not None:
+            return input("Your answer: ").strip()
+        else:
+            return None  # No need to get an answer for answers
 
 class ScoreKeeper:
     def __init__(self):
         self.player_score = 0
         self.computer_score = 0
 
+    # def get_player_score(self, player_answer, correct_answer):
+    #     print(f"Player Answer: {player_answer}")
+    #     if correct_answer is not None:
+    #         print(f"Correct Answer: {correct_answer.lower()}")
+    #         if player_answer and player_answer.lower() == correct_answer.lower():
+    #             self.player_score += 1
+    #             print(f"You now have {self.player_score} points!")
+    #         else:
+    #             print(f"You now have {self.player_score} points.")
+    #     else:
+    #         print("Invalid question format. Cannot determine correct answer.")
+
+    # def get_computer_score(self, correct_answer):
+    #     if correct_answer is not None:
+    #         computer_answer = random.choice(['option1', 'option2', 'option3'])
+    #         print(f"Computer's answer: {computer_answer}, Correct: {computer_answer.lower() == correct_answer.lower()}")
+    #         if computer_answer.lower() == correct_answer.lower():
+    #             self.computer_score += 1
+    #             print(f"Computer now has {self.computer_score} points!")
+    #         else:
+    #             print(f"Computer now has {self.computer_score} points.")
+    #     else:
+    #         print("Invalid question format. Cannot determine correct answer.")
+    
     def get_player_score(self, player_answer, correct_answer):
-        if player_answer == correct_answer:
-            self.player_score += 1
-            print(f"You now have {self.player_score} points!")
+        print(f"Player Answer: {player_answer}")
+        if correct_answer is not None:
+            print(f"Correct Answer: {correct_answer}")
+            if player_answer == correct_answer:
+                self.player_score += 1
+                print(f"Correct! You now have {self.player_score} points!")
+            else:
+                print(f"Sorry, the correct answer is {correct_answer}. You still have {self.player_score} points.")
         else:
-            print(f"You now have {self.player_score} points.")
+            print("Invalid question format. Cannot determine the correct answer.")
 
     def get_computer_score(self, correct_answer):
-        # Implement logic for computer's answer (random or predetermined)
-        computer_answer = random.choice(['option1', 'option2', 'option3'])
-        if computer_answer == correct_answer:
-            self.computer_score += 1
-            print(f"Computer now has {self.computer_score} points!")
+        if correct_answer is not None:
+            computer_answer = random.choice([correct_answer, correct_answer, correct_answer])
+            print(f"Computer's answer: {computer_answer}, Correct: {computer_answer == correct_answer}")
+            if computer_answer == correct_answer:
+                self.computer_score += 1
+                print(f"Computer now has {self.computer_score} points!")
+            else:
+                print(f"Computer now has {self.computer_score} points.")
         else:
-            print(f"Computer now has {self.computer_score} points.")
+            print("Invalid question format. Cannot determine the correct answer.")
 
     def display_score(self):
         print(f"Player: {self.player_score} | Computer: {self.computer_score}")
@@ -66,26 +112,34 @@ class Timer:
         time_passed = self.end_time - self.start_time
         print(f"Time elapsed: {time_passed} seconds")
 
+def run_game(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line_number, line in enumerate(f, start=1):
+            try:
+                game = TriviaGame(line.strip())
+            except ValueError as e:
+                print(f"Error on line {line_number}: {e}")
+            else:
+                game.display_question()
+                timer.start()
+                if game.answer is not None:
+                    score_keeper.get_computer_score(game.answer)
+                else:
+                    player_answer = game.get_answer()
+                    timer.end()
+                    score_keeper.get_player_score(player_answer, game.answer)
+
+                score_keeper.display_score()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Trivia Game')
     parser.add_argument('file_path', help='Path to the file containing trivia questions.')
     args = parser.parse_args()
     file_path = args.file_path
 
-    with open(file_path, 'r', encoding='utf-8') as f:
-        games = [TriviaGame(line.strip()) for line in f]
-
     score_keeper = ScoreKeeper()
     timer = Timer()
 
-    for game in games:
-        game.display_question()
-        timer.start()
-        player_answer = game.get_answer()
-        timer.end()
-
-        score_keeper.get_player_score(player_answer, game.answer)
-        score_keeper.get_computer_score(game.answer)
-        score_keeper.display_score()
+    run_game(file_path)
 
     score_keeper.determine_winner()
